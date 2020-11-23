aa75e4005cab215e4927ad2583a7688c7c7366a2
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2020-11-23 14:46:42 -0800

    chore(release): make changelog generation conditional and fix branch order

--- release.config.js
+++ release.config.js
@@ -1,12 +1,16 @@
+const SHOULD_UPDATE_CHANGELOG = process.env.SHOULD_UPDATE_CHANGELOG;
+// eslint-disable-next-line no-console
+console.info(`SHOULD_UPDATE_CHANGELOG=${SHOULD_UPDATE_CHANGELOG} (${typeof SHOULD_UPDATE_CHANGELOG})`);
+
 module.exports = {
     //extends: '@xunnamius/semantic-release-config',
     branches: [
-        'main',
         '+([0-9])?(.{+([0-9]),x}).x',
+        'main',
         {
-            name: 'develop',
-            channel: 'next',
-            prerelease: 'canary',
+            name: 'canary',
+            channel: 'canary',
+            prerelease: true,
         }
     ],
     plugins: [
@@ -14,12 +18,14 @@ module.exports = {
             releaseRules: [{ type: 'build', release: 'patch' }]
         }],
         '@semantic-release/release-notes-generator',
-        ['@semantic-release/changelog', {
+        ...(SHOULD_UPDATE_CHANGELOG === 'true' ?
+        [['@semantic-release/changelog', {
             changelogTitle: `# Changelog\n\n> [See a summary of upcoming changes](${require('./package.json').repository.url}/compare/main...develop)\n\nAll notable changes to this project will be documented in this file.\n\nThe format is based on [Conventional Commits](https://conventionalcommits.org),\nand this project adheres to [Semantic Versioning](https://semver.org).\n\n`,
-        }],
+        }]] : []),
         '@semantic-release/npm',
         ['@semantic-release/git', {
             assets: ['package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'CHANGELOG.md', 'docs'],
+            message: "release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github',
     ],
