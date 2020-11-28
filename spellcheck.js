14b86ae815f2c24aef82dcad41b0ead036a2f4c4
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2020-11-28 03:03:22 -0800

    fix(spellcheck): spellcheck now leverages assorted knowledge
    Reads from:
    ./.spellcheckignore
    ${homeDir}/.config/_spellcheckignore
    ./.vscode/settings.json
    ${homeDir}/.config/Code/User/settings.json
    
    Also much more resilient to errors and more reliably filters out redundant/empty/bad text

--- spellcheck.js
+++ spellcheck.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-console */
 const spellcheck = require('spellchecker');
+const pkg = require('./package.json');
 const read = require('fs').promises.readFile;
+const sjx = require('shelljs');
+
+sjx.config.silent = true;
 
 const tryToRead = async (path) => {
   try {
     return await read(path);
   } catch (ignored) {
+    console.log(ignored);
     return '';
   }
 };
@@ -13,56 +18,77 @@ const tryToRead = async (path) => {
 const asJson = (str) => {
   try {
     const json = JSON.parse(str.toString('utf-8'));
-    return [...(json?.['cSpell.words'] || []), ...(json?.['cSpell.ignoreWords'] || [])];
+    return [
+      ...(json?.['cSpell.words'] || []),
+      ...(json?.['cSpell.ignoreWords'] || [])
+    ];
   } catch (ignored) {
     return [];
   }
 };
 
-const asText = (str) => {
-  return str
-    .toString('utf-8')
-    .split('\n')
-    .map((word) => word.trim())
-    .filter((word) => !!word);
+const asText = (str) => str.toString('utf-8').split('\n');
+const sanitizeWord = (word) => word.replace(/[^a-zA-Z0-9'\w]/g, ' ');
+
+const splitOutWords = (phrase) => {
+  return [...phrase.split(/(?:[^\w\-_']|\s)\s*/g), phrase]
+    .map((a) => sanitizeWord(a.trim()))
+    .filter((a) => !!a);
 };
 
-const sanitize = (word) => word.replace(/[^a-zA-Z0-9']/g, ' ');
+const keys = (obj) => Object.keys(obj).map(splitOutWords).flat();
 
 (async () => {
   const lastCommitMsg = await require('last-commit-message')();
+  const homeDir = require('os').homedir();
 
   const ignoreWords = Array.from(
     new Set(
-      (
-        await Promise.all([
-          await require('text-extensions'),
+      [
+        ...(await Promise.all([
           tryToRead('./.spellcheckignore').then(asText),
-          tryToRead('~/.config/_spellcheckignore').then(asText),
+          tryToRead(`${homeDir}/.config/_spellcheckignore`).then(asText),
           tryToRead('./.vscode/settings.json').then(asJson),
-          tryToRead('~/.config/Code/User/settings.json').then(asJson)
-        ])
-      ).flat()
+          tryToRead(`${homeDir}/.config/Code/User/settings.json`).then(asJson)
+        ])),
+        ...require('text-extensions'),
+        ...keys(pkg.dependencies).map(splitOutWords),
+        ...keys(pkg.devDependencies).map(splitOutWords),
+        ...keys(pkg.scripts).map(splitOutWords),
+        ...sjx
+          .exec('git log --format="%B"')
+          .stdout.split(/(?:[^\w\-_']|\s)\s*/g)
+      ]
+        .flat()
+        .filter(Boolean)
+        .map((word) => word.trim().toLowerCase())
     )
   );
 
-  let spoken = false;
   const seen = [];
-  const typoLocations = spellcheck.checkSpelling(lastCommitMsg);
 
-  if (typoLocations.length) {
-    for (const typoLocation of typoLocations) {
-      if (spoken) {
-        spoken = true;
-        console.warn('WARNING: there may be misspelled words in your commit message!');
-        console.warn('These can be fixed before push with `git commit -S --amend`');
-        console.warn('---');
-      }
+  const typos = spellcheck
+    .checkSpelling(lastCommitMsg)
+    .map((typoLocation) => {
+      return lastCommitMsg
+        .slice(typoLocation.start, typoLocation.end)
+        .toLowerCase();
+    })
+    .filter(
+      (typo) =>
+        !ignoreWords.includes(typo) && !ignoreWords.includes(sanitizeWord(typo))
+    );
 
-      const typo = lastCommitMsg.slice(typoLocation.start, typoLocation.end).toLowerCase();
+  if (typos.length) {
+    console.warn(
+      'WARNING: there may be misspelled words in your commit message!'
+    );
+    console.warn(
+      'Commit messages can be fixed before push with `git commit -S --amend`'
+    );
+    console.warn('---');
 
-      if (ignoreWords.includes(typo) || ignoreWords.includes(sanitize(typo))) continue;
-
+    for (const typo of typos) {
       if (!seen.includes(typo)) {
         const corrections = spellcheck.getCorrectionsForMisspelling(typo);
         const suggestion = corrections.length
@@ -76,6 +102,6 @@ const sanitize = (word) => word.replace(/[^a-zA-Z0-9']/g, ' ');
       if (seen.length == 5) break;
     }
 
-    typoLocations.length > 5 && console.warn(`${typoLocations.length - 5} more...`);
+    typos.length > 5 && console.warn(`${typos.length - 5} more...`);
   }
 })();
