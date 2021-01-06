3973279927772eea45ac8573c1a91dea3635c6eb
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-01-06 06:07:45 -0800

    build(webpack.config.js): process.env shim

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 // This webpack config is used to transpile src to dist, compile externals, etc
 
-const debug = require('debug')(`${require('./package.json').name}:webpack-config`);
-const { verifyEnvironment } = require('./env-expect');
+const { EnvironmentPlugin, DefinePlugin } = require('webpack');
 const { config: populateEnv } = require('dotenv');
+const { verifyEnvironment } = require('./env-expect');
+const nodeExternals = require('webpack-node-externals');
+const debug = require('debug')(`${require('./package.json').name}:webpack-config`);
 
-populateEnv();
+const dotenv = populateEnv();
+debug('saw dotenv result => %O', dotenv);
+const env = dotenv.parsed || {};
+debug('got env => %O', env);
 verifyEnvironment();
+
+const plugins = [
+  // ? Load our .env results as the defaults
+  new EnvironmentPlugin(env),
+  // ? Create a shim for process.env (sorry but I like using it everywhere!)
+  new DefinePlugin({
+    ...(process.env.DEBUG ? { 'process.env.DEBUG': `"${process.env.DEBUG}"` } : {}),
+    'process.env': '{}'
+  })
+];
 
 module.exports = {
   name: 'main',
@@ -28,15 +43,16 @@ module.exports = {
     usedExports: true
   },
 
-  //externals: [nodeExternals()],
-  //externalsPresets: { node: true },
+  externals: [nodeExternals()],
+  externalsPresets: { node: true },
 
   resolve: { extensions: ['.ts', '.wasm', '.mjs', '.cjs', '.js', '.json'] },
   module: {
     rules: [{ test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ }]
   },
   optimization: { usedExports: true },
-  ignoreWarnings: [/critical dependency:/i]
+  ignoreWarnings: [/critical dependency:/i],
+  plugins
 };
 
 debug('exports = %O', module.exports);
